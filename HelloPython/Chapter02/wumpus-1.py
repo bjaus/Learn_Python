49ffc3a4cce318135d740b73cb4d0750bbc937be
Brandon Jaus <brandon.jaus@gmail.com>
2016-04-04 09:47:59 -0500

    added wumpus-1.py

---
+++ HelloPython/Chapter02/wumpus-1.py
@@ -0,0 +1,29 @@
+from random import choice
+
+cave_numbers = range(1,21)
+wumpus_location = choice(cave_numbers)
+player_location = choice(cave_numbers)
+while player_location == wumpus_location:
+	player_location = choice(cave_numbers) 
+	
+print("Welcome to Hunt the Wumpus!")
+print("You can see {} caves".format(len(cave_numbers)))
+print("To play, just type the number")
+print("of the cave you wish to enter next")
+
+while True:
+	print("You are in cave {}".format(player_location))
+	if (player_location == wumpus_location - 1 or
+		player_location == wumpus_location + 1):
+		print("I smell a wumpus!")
+		
+	print("Which cave next?")
+	player_input = input(">")
+	if (not player_input.isdigit() or
+		int(player_input) not in cave_numbers):
+		print("{} is not a cave!".format(player_input))
+	else:
+		player_location = int(player_input)
+		if player_location == wumpus_location:
+			print("Aargh! You got eaten by a wumpus!")
+			break
